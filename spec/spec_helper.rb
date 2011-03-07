626d58909e3e86c0b3f69248703e85bc15b39df9
John Griffin <johnog@gmail.com>
2011-03-07 23:28:49 +0000

    upgrade to latest rspec and fakeweb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'mechanize_content'
-require 'spec'
-require 'spec/autorun'
 require "fakeweb"
 FakeWeb.allow_net_connect = false
-
-Spec::Runner.configure do |config|
-
-end
