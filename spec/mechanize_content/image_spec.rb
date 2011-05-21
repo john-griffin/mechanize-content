4312c9486db3a5078b741740de241f19b8f66b97
John Griffin <johnog@gmail.com>
2011-05-21 09:41:37 +0100

    wrap up the image specs

--- spec/mechanize_content/image_spec.rb
+++ spec/mechanize_content/image_spec.rb
@@ -1,35 +1,37 @@
 require 'spec_helper'
 
 describe MechanizeContent::Image do
-  context "given a gif" do
-    it "is not interesting" do
-      img = {"src" => "http://www.cmpevents.com/GD10/ablank.gif2", "width" => 500, "height" => 500}
-      image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
-      image.should_not be_interesting_css    
+  describe "#interesting_css?" do
+    context "given a gif" do
+      it "is not interesting" do
+        img = {"src" => "http://www.cmpevents.com/GD10/ablank.gif2", "width" => 500, "height" => 500}
+        image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
+        image.should_not be_interesting_css    
+      end
     end
-  end
-  
-  context "given a banner" do
-    it "is not interesting" do
-      img = {"src" => "http://www.cmpevents.com/GD10/banner.png", "width" => 500, "height" => 500}
-      image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
-      image.should_not be_interesting_css    
+    
+    context "given a banner" do
+      it "is not interesting" do
+        img = {"src" => "http://www.cmpevents.com/GD10/banner.png", "width" => 500, "height" => 500}
+        image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
+        image.should_not be_interesting_css    
+      end
     end
-  end
-  
-  context "given a small image" do
-    it "is not interesting" do
-      img = {"src" => "http://www.cmpevents.com/GD10/toosmall.png", "width" => 64, "height" => 500}
-      image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
-      image.should_not be_interesting_css
-    end
-  end
 
-  context "given a well sized non advertising image" do
-    it "is valid" do
-      img = {"src" => "http://www.cmpevents.com/GD10/perfecto.png", "width" => 500, "height" => 500}
-      image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
-      image.should be_interesting_css
+    context "given a small image" do
+      it "is not interesting" do
+        img = {"src" => "http://www.cmpevents.com/GD10/toosmall.png", "width" => 64, "height" => 500}
+        image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
+        image.should_not be_interesting_css
+      end
     end
-  end
+
+    context "given a well sized non advertising image" do
+      it "is valid" do
+        img = {"src" => "http://www.cmpevents.com/GD10/perfecto.png", "width" => 500, "height" => 500}
+        image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
+        image.should be_interesting_css
+      end
+    end
+  end  
 end
