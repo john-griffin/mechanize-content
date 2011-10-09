0c0c8ccaa0f29b401a0a31bf59d1419e5a22dd86
John Griffin <johnog@gmail.com>
2011-10-09 22:40:12 +0100

    image source uri should be escaped

--- lib/mechanize_content/image.rb
+++ lib/mechanize_content/image.rb
@@ -4,35 +4,35 @@ module MechanizeContent
     MIN_HEIGHT = 64
     AD_WIDTH = 728
     AD_HEIGHT = 90
-    
+
     def self.best_image(images, base_url)
       imgs = images.map{|i| Image.new(i, base_url)}
       top_image = imgs.select{|i| i.interesting_css?}.first || imgs.select{|i| i.interesting_file?}.first
       top_image.absolute_url if top_image
     end
-    
+
     def initialize(image, base_url)
-      @src      = image["src"]
+      @src      = URI.escape(image["src"])
       @width    = image["width"].to_i
       @height   = image["height"].to_i
       @base_url = base_url
     end
-    
+
     def interesting_css?
       valid_image?(@width, @height)
     end
-    
+
     def interesting_file?
       open(absolute_url, "rb") do |fh|
         is = ImageSize.new(fh.read)
         return valid_image?(is.width, is.height)
       end
     end
-    
+
     def valid_image?(width, height)
       big_enough?(width, height) && not_advertising?(width, height) && allows_hotlinking?
     end
-    
+
     def allows_hotlinking?
       begin
         open(absolute_url, "Referer" => "http://splitstate.com")
@@ -41,21 +41,21 @@ module MechanizeContent
       end
       true
     end
-    
+
     def advertising?(width, height)
       @src.include?("banner") || @src.include?(".gif") || ((width == AD_WIDTH) && (height == AD_HEIGHT))
     end
-    
+
     def not_advertising?(width, height)
       !advertising?(width, height)
     end
-    
+
     def big_enough?(width, height)
       width > MIN_WIDTH && height > MIN_HEIGHT
     end
-    
+
     def absolute_url
       URI.parse(@src).relative? ? (URI.parse(@base_url.to_s)+@src).to_s : @src
-    end    
+    end
   end
 end
--- spec/mechanize_content/image_spec.rb
+++ spec/mechanize_content/image_spec.rb
@@ -2,21 +2,31 @@ require 'spec_helper'
 
 describe MechanizeContent::Image do
   use_vcr_cassette :record => :new_episodes
-  
+
+  describe "#absolute_url" do
+    context "given a uri with a space" do
+      it "will be escaped" do
+        img = {"src" => "http://media.giantbomb.com/uploads/0/26/10180-psn icon_middle.jpg", "width" => 280, "height" => 283}
+        image = MechanizeContent::Image.new(img, "http://www.giantbomb.com/news/the-slow-motion-ballet-of-death-in-max-payne-3/3721/")
+        image.absolute_url.should eq("http://media.giantbomb.com/uploads/0/26/10180-psn%20icon_middle.jpg")
+      end
+    end
+  end
+
   describe "#interesting_css?" do
     context "given a gif" do
       it "is not interesting" do
         img = {"src" => "http://www.cmpevents.com/GD10/ablank.gif2", "width" => 500, "height" => 500}
         image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
-        image.should_not be_interesting_css    
+        image.should_not be_interesting_css
       end
     end
-    
+
     context "given a banner" do
       it "is not interesting" do
         img = {"src" => "http://www.cmpevents.com/GD10/banner.png", "width" => 500, "height" => 500}
         image = MechanizeContent::Image.new(img, "https://www.cmpevents.com")
-        image.should_not be_interesting_css    
+        image.should_not be_interesting_css
       end
     end
 
@@ -35,5 +45,5 @@ describe MechanizeContent::Image do
         image.should be_interesting_css
       end
     end
-  end  
+  end
 end
